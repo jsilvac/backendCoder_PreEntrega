b639db66f1187198b6c67ed44403c16d29bb2e1a
jaime <silvacastillojaime@gmail.com>
2024-01-29 00:44:41 -0300

    MOdificado del producto ok, post carrito, falta mover el cartmanager y hacer pruebas

--- routes/carts.router.js
+++ routes/carts.router.js
@@ -1,48 +1,185 @@
 import exprees from 'express'
+import ProductManager from '../controller/ProductManager.js';
 
 const router = exprees.Router();
 
 // clases
-class ProductManager {
+class CartManager {
 
     constructor() {
     this.filePath = "carrito.json";
     }
 
+    async checkFileExist() {
+        try {
 
-    async checkFileExist(){
-        try{
-            await fs.access(this.filePath);
+            await fs.promises.stat(this.filePath);
             return true;
-        }catch(error){
-            return false
+        } catch (error) {
+            return false;
         }
     }
 
     async createFile() {
         try {
-            await fs.promises.writeFile(this.filePath, '[]');
+            await fs.promises.writeFile(this.filePath, `[
+                {"product":[],}
+            ]`);
             console.log("Archivo creado: ", this.filePath);
         } catch (error) {
             console.log("Error al crear el archivo: " + error);
         }
     }
 
-    async getCartById(idProduct) {
-        const products = await this.getProducts();
-
-        const product = products.find(item => item.id === idProduct);
-
-        return product ? product : null;
+    async getCarritos() {
+        try {
+            const fileExists = await this.checkFileExist();
+            
+            if (!fileExists) {
+                await this.createFile();
+            }
+    
+            const data = await fs.promises.readFile(this.filePath, "utf8");
+            
+            return data ? JSON.parse(data) : [];
+    
+        } catch (error) {
+            console.log("Error fetching en carritos:", error);
+            return [];
+        }
     }
+
+    async getCartById(idCarrito) {
+
+        try{
+
+            const carrito = await this.getCarritos();
+    
+            const carro = carrito.find(cart => cart.id === idCarrito);
+    
+            return carro ? carro : null;
+
+        }catch(error){
+            console.log("Error fetching en carrito:", error);
+            return [];
+        }
+    }
+
+    async addNewProductCart(cart, product) {
+        try {
+            let cartExist = false;
+    
+            for (let i = 0; i < cart.length; i++) {
+                if (cart[i].id === product.id_carrito) {
+                    cartExist = true;
+    
+                    let productExist = false;
+    
+                    
+                    for (let j = 0; j < cart[i].productos.length; j++) {
+                        if (cart[i].productos[j].id_producto === product.id_producto) {
+                            productExist = true;
+                            cart[i].productos[j].stock += 1;  
+                        }
+                    }
+    
+                    if (!productExist) {
+                        
+                        cart[i].productos.push({
+                            id_producto: product.id_producto,
+                            stock: 1
+                        });
+                    }
+                }
+            }
+    
+            if (!cartExist) {
+                // Si el carrito no existe, asignar un nuevo id basado en el máximo existente
+                const maxCartId = Math.max(...cart.map(cartItem => cartItem.id), 0);
+                const newCartId = maxCartId + 1;
+    
+                cart.push({
+                    id: newCartId,
+                    productos: [{
+                        id_producto: product.id_producto,
+                        stock: 1
+                    }]
+                });
+            }
+    
+            await fs.promises.writeFile(this.filePath, JSON.stringify(cart), 'utf8');
+    
+        } catch (error) {
+            console.error('Error:', error);
+            throw new Error('Error del servidor');
+        }
+    }
+    
+    
 }
 
-router.get("/api/cart/:pid", (req,res) => {
-    res.send(' cart get')
+router.get("/api/cart/:cid", async (req,res) => {
+    
+    const xId = req.params.pid;
+    const prodMan = new ProductManager();
+    
+    try{
+
+        const carrito = await prodMan.getCartById(xId);
+ 
+        if(!carrito){
+            res.status(404).send("Carrito no encontrado");
+            return
+        }
+        
+        res.status(200).json(carrito);
+        
+    }catch(error){
+        console.error('Error:', error);
+        res.status(500).send('Error del servidor');
+    }
 })
 
-router.post("/api/cart", (req,res) => {
-    res.send('cart post')
+router.post("/api/cart/:cid/product/:pid", async  (req,res) => {
+   
+    try{
+
+        const newCart = req.body;
+
+        if (!newCart || Object.keys(newCart).length === 0) {
+            res.status(400).json({ error: "El objeto de carito está vacío o no es válido." });
+            return;
+        }
+
+        const CartMan = new CartManager();
+        const cUId = req.params.pid
+
+        const prodMan = new ProductManager();
+        const pUId = req.params.pid;
+
+        const product = await prodMan.getCartById(pUId);
+        const cart = await CartMan.getCartById(cUId);
+
+        if(!cart){
+            res.status(404).send("Carrito no encontrado");
+            return
+        }
+        
+        if(!product){
+            res.status(404).send("Producto no encontrado");
+            return
+        }
+
+        //Si el id del producto y del carrito son diferentes, hacemos una comprobación adicional para asegurarnos que el producto no esté ya
+  
+        const NewProduct = await CartMan.addNewProductCart(cart,product)
+
+        res.status(200).json(cart);
+        
+    }catch(error){
+        console.error('Error:', error);
+        res.status(500).send('Error del servidor');
+    }
 })
 
 router.put("/api/cart/:pid", (req,res) => {
--- routes/products.router.js
+++ routes/products.router.js
@@ -1,161 +1,9 @@
 import express from 'express'
-import fs from 'fs'
-import { v4 as uuidv4 } from 'uuid';
+import ProductManager from '../controller/ProductManager.js';
 
 const router = express.Router();
 
 const products = [];
-
-// clases
-class ProductManager {
-
-    constructor() {
-        this.filePath = "productos.json";
-    }
-
-    async checkFileExist() {
-        try {
-
-            await fs.promises.stat(this.filePath);
-            return true;
-        } catch (error) {
-            return false;
-        }
-    }
-    
-
-    async createFile() {
-        try {
-            await fs.promises.writeFile(this.filePath, '[]');
-            console.log("Archivo creado: ", this.filePath);
-        } catch (error) {
-            console.log("Error al crear el archivo: " + error);
-        }
-    }
-  
-    async getProducts() {
-        try {
-            const fileExists = await this.checkFileExist();
-            
-            if (!fileExists) {
-                await this.createFile();
-            }
-    
-            const data = await fs.promises.readFile(this.filePath, "utf8");
-            
-            return data ? JSON.parse(data) : [];
-    
-        } catch (error) {
-            console.log("Error fetching en productos:", error);
-            return [];
-        }
-    }
-    
-
-    async addProduct(product) {
-
-        try {
-            
-            const existingProducts = await this.getProducts();
-            console.log("esto esta en el json", existingProducts)
-            
-            const existingProductIndex = existingProducts.findIndex(p => p.nombre === product.nombre);
-    
-            if (existingProductIndex) {
-                
-                const { stock, ...existingProduct } = existingProducts[existingProductIndex];
-                existingProducts[existingProductIndex] = { ...existingProduct, stock: stock + 1 };
-            } else {
-                
-                const newProduct = { ...product, id: uuidv4(), stock: 1 };
-                console.log("nuevo producto", newProduct)
-                existingProducts.push(newProduct);
-            }
-    
-            await fs.promises.writeFile(this.filePath, JSON.stringify(existingProducts), 'utf8');
-    
-            return existingProductIndex 
-                ? existingProducts[existingProductIndex]
-                : existingProducts[existingProducts.length - 1];
-    
-        } catch (error) {
-            console.error('Error:', error);
-            throw new Error('Error del servidor'); 
-        }
-    }
-    
-    async updateProduct(productID, updateData) {
-        try {
-            const existingProducts = await this.getProducts();
-            console.log("Antes de la actualización:", JSON.stringify(existingProducts, null, 2));
-
-            const existingProductIndex = existingProducts.findIndex(p => p.id === productID);
-
-            if (existingProductIndex >= 0) {
-                console.log("entro al update", updateData)
-
-                const PordExist = existingProducts[existingProductIndex];
-
-                for (const key in updateData) {
-                    if (updateData.hasOwnProperty(key)) {
-                        PordExist[key] = updateData[key];
-                    }
-                }
-                existingProducts[existingProductIndex] = PordExist;
-
-                await fs.promises.writeFile(this.filePath, JSON.stringify(existingProducts), 'utf8');
-                
-                return existingProducts[existingProductIndex];
-            } else {
-                throw new Error('Producto no encontrado');
-            }
-            console.log("no entro")
-        } catch (error) {
-            console.error('Error:', error);
-            throw new Error('Error del servidor');
-        }
-    }
-    
-    async getProductById(idProduct) {
-
-        try{
-            
-            const products = await this.getProducts();   
-            const product = products.find(item => item.id === idProduct);
-
-            return product ? product : null;
-
-        }catch(error){
-            console.log("Error fetching en productos:", error);
-            return [];
-        }
-    }
-
-    async deleteProduct(productID) {
-
-        try {
-            const existingProducts = await this.getProducts();
-            
-            console.log("produc id: ", productID)
-            const existingProductIndex = existingProducts.findIndex(p => p.id === productID);
-            console.log("exist id", existingProductIndex)
-            if (existingProductIndex >= 0) {
-
-                existingProducts.splice(existingProductIndex, 1);
-    
-                await fs.promises.writeFile(this.filePath, JSON.stringify(existingProducts), 'utf8');
-    
-                return { message: 'Producto eliminado correctamente' };
-            } else {
-                throw new Error('Producto no encontrado!');
-            }
-        } catch (error) {
-            console.error('Error:', error);
-            throw new Error('Error del servidor');
-        }
-    }
-    
-}
 
 // Enpoint 
 router.get("/api/products", async (req, res) => {
@@ -216,6 +64,11 @@ router.post("/api/products", async (req,res) => {
     try{
 
         const newProduct = req.body;
+        
+        if (!newProduct || Object.keys(newProduct).length === 0) {
+            res.status(400).json({ error: "El objeto de producto está vacío o no es válido." });
+            return;
+        }
 
         if(!newProduct){
             res.status(404).send("Producto no creado!");
